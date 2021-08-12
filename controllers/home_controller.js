1b4a86d26611f13ba30cf25a6dd06c440360dab9
Eric Culley <eric.culley@auth0.com>
2021-08-12 17:52:58 -0600

    added idToken and protected endpoint

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -8,11 +8,12 @@ home.get('/', (req, res) => {
     res.render('index.ejs', {
         userInfo: req.oidc.user,
         isAuthenticated: req.oidc.isAuthenticated(),
+        idToken: req.oidc.idTokenClaims,
     })
 })
 
-home.get('/status', requiresAuth(), (req, res) => {
-    res.send("This is the status page, super secret info")
+home.get('/requires-auth', requiresAuth(), (req, res) => {
+    res.send("!!!This page requires authorization because it stores super secret info!!!")
 })
 
 // Export Router
